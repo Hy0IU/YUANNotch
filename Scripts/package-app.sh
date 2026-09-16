ffb7d655cb40155ea80318a622096f0a6f94445a
Hy0IU <202503021032t@stu.cqu.edu.cn>
2026-09-16 17:08:21 +0800

    chore: bump the bundle version to 0.3.7 (build 11)

--- Scripts/package-app.sh
+++ Scripts/package-app.sh
@@ -53,9 +53,9 @@ cat > "$CONTENTS_DIR/Info.plist" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.3.6</string>
+  <string>0.3.7</string>
   <key>CFBundleVersion</key>
-  <string>10</string>
+  <string>11</string>
   <key>LSMinimumSystemVersion</key>
   <string>14.0</string>
   <key>LSUIElement</key>
